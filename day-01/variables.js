e6d9e38701dcff95510164d9d9912069274f697f
Mehedi Hasan Likhon <mehedi.hasansjs@gmail.com>
2020-02-02 20:06:43 +0600

    Create variables.js

---
+++ day-01/variables.js
@@ -0,0 +1,31 @@
+/*
+  (c) Copyright - Mehedi Hasan Likhon
+  (c) Copyright - DIU CPC
+  Date: 02 February 2020
+*/
+
+// Variables
+
+var myName = 'mehedi hasan likhon';
+
+// Here the keyword 'var' is used to define a variable in JS
+// you can put anything in a variable including string, number, or anything...
+// literally anything!
+
+var myString = 'I love JS';
+var myNumber = 10;
+
+// Here you can put whole object into variable
+var myObject = {
+  id: 13,
+  name: 'My Lovely Object'
+};
+
+// Here you can put function in a variable
+var myFunction = function() {
+  return `this is a cute function!`;
+};
+
+// Remember, you must write your variable name in camleCase
+// So, you need to write the first word in small letter and
+// rest of the word in Capitalize Each Word style
